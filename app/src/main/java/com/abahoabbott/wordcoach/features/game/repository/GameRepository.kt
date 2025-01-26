38303b8bddd3310f92d804af461587bc3bac19be
Abaho Abbott Strange <2020mbr001@std.must.ac.ug>
2025-01-26 07:39:05 +0300

    feat(game): Implement cumulative score persistence
    - Added functions to save, retrieve, and reset the cumulative score using DataStore.
    - The score is stored with the key "cumulative_score" in the DataStore.
    - Integrated the score persistence logic into the GameRepository.

--- app/src/main/java/com/abahoabbott/wordcoach/features/game/repository/GameRepository.kt
+++ app/src/main/java/com/abahoabbott/wordcoach/features/game/repository/GameRepository.kt
@@ -1,4 +1,37 @@
 package com.abahoabbott.wordcoach.features.game.repository
 
-class GameRepository {
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import com.abahoabbott.wordcoach.features.game.GameViewModel
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GameRepository @Inject constructor(
+    private val dataStore: DataStore<Preferences>,
+) {
+
+    suspend fun saveCumulativeScore(score: Int) {
+        dataStore.edit { preferences ->
+            preferences[SCORE_KEY] = score
+        }
+    }
+    suspend fun getCumulativeScore(): Int {
+        return dataStore.data.map { preferences ->
+            preferences[SCORE_KEY] ?: 0
+        }.first()
+    }
+
+    suspend fun resetCumulativeScore() {
+        dataStore.edit { preferences ->
+            preferences[SCORE_KEY] = 0
+        }
+    }
+    companion object{
+         private val SCORE_KEY = intPreferencesKey("cumulative_score")
+
+    }
+
 }
